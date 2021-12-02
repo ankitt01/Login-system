d202e9ea962fbd64fc3a6230307838a2ab1befd6
ankitt01 <ankitrawat0102@gmail.com>
2021-12-02 08:19:31 +0530

    Logout component's jsx and implementing redux

---
+++ src/components/Logout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logout() {
+    return (
+        <div>
+            <h1>Welcome <span className="user__name">Ankit</span>
+            <button className="logout__button">Logoutgit</button>
+            </h1>
+        </div>
+    )
+}
+
+export default Logout
