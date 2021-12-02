e3161943974906c63ef4c1ab50c889140dfd74ad
ankitt01 <ankitrawat0102@gmail.com>
2021-12-02 09:02:55 +0530

    styling

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import './Login.css'
 
 function Logout() {
     return (
-        <div>
-            <h1>Welcome <span className="user__name">Ankit</span>
+        <div className="logout">
+            <h1>Welcome <span className="user__name">Ankit</span></h1>
             <button className="logout__button">Logout</button>
-            </h1>
         </div>
     )
 }
