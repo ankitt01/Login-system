a8ec162dea9395a064d1ebd203b9f06c8f4188eb
ankitt01 <ankitrawat0102@gmail.com>
2021-12-02 09:08:35 +0530

    logout functionality

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import {logout} from '../features/userSlice'
 import './Login.css'
 
 function Logout() {
+    const dispatch = useDispatch(logout())
+
+    const handleLogout = (e) => {
+        e.preventDefault()
+        dispatch(logout())
+    }
     return (
         <div className="logout">
             <h1>Welcome <span className="user__name">Ankit</span></h1>
-            <button className="logout__button">Logout</button>
+            <button className="logout__button" onClick={(e) => handleLogout(e)}>Logout</button>
         </div>
     )
 }
